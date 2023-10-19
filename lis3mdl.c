8bbc091a33989e01280c4f817f2a96e5abdaa0b6
mvari1 <144389101+mvari1@users.noreply.github.com>
2023-10-18 20:56:28 -0400

    Initial LIS3MDL Driver Implementation
    Description:
    This commit introduces an initial implementation of the LIS3MDL driver for CubeIDE. It includes basic read and write functionality for sensor data and configuration registers. The code has been tested on the Nucleo-L412KB development board, and it is functional in our testing environment.
    
    Known Issues:
    - There is an issue with the conversion from raw sensor data to meaningful data. The current conversion logic may result in inaccurate or incorrect sensor readings. Further investigation and adjustment of the data conversion code are needed to address this issue.
    
    Note: While the driver is currently tailored for the Nucleo-L412KB board, it can be made compatible with other boards by including the appropriate header file in the `.h` file.

---
+++ lis3mdl.c
@@ -0,0 +1,140 @@
+#include "lis3mdl.h"
+
+uint8_t	LIS3MDL_Initialize(LIS3MDL *dev, I2C_HandleTypeDef *i2cHandle){
+
+	/* Set struct parameters */
+	dev->i2cHandle 		= i2cHandle;
+
+	dev->mag_guass[0]		= 0.0f;
+	dev->mag_guass[1]		= 0.0f;
+	dev->mag_guass[2]		= 0.0f;
+
+
+	dev->temp_c				= 0.0f;
+
+	/* Store number of errors (to be returned at the end of the function) */
+	uint8_t	errNum = 0;
+	HAL_StatusTypeDef status;
+
+	/*
+	 * Check device ID is correct (pg. 24)
+	 */
+	uint8_t regData;
+
+	status = LIS3MDL_ReadRegister( dev, WHO_AM_I, &regData);
+	errNum += (status != HAL_OK);
+
+	if( regData != LIS3MDL_DEVICE_ID){
+
+		return 255;
+	}
+
+	/* 
+	 * Enable the temperature sensor (p. 24) 
+	 */
+	regData = 0x90;
+
+	status = LIS3MDL_WriteRegister(dev, CTRL_REG1, &regData);
+	errNum += (status != HAL_OK );
+
+
+	/* Checking it wrote to the register */
+	status = LIS3MDL_ReadRegister(dev, CTRL_REG1, &regData);
+	errNum += (0x90 != regData );
+
+	/* 
+	 * Put sensor in to measurement mode (p. 26) 
+	 */
+	regData = 0x00;
+
+	status = LIS3MDL_WriteRegister(dev, CTRL_REG3, &regData);
+	errNum += (status != HAL_OK );
+
+	/* Checking it wrote to the register */
+	status = LIS3MDL_ReadRegister(dev, CTRL_REG3, &regData);
+	errNum += (0x00 != regData );
+
+	/* Return number of erros (0 if successful initialization) */
+	return errNum;
+
+}
+
+/*
+ * DATA AQUISITION
+ */
+HAL_StatusTypeDef LIS3MDL_ReadTemperature(LIS3MDL *dev){
+	/*	DATASHEET PAGE 9 */
+
+	/*
+	 * Read raw values from temperature registers (16 bits two's complement)
+	 */
+	uint8_t regData[2];
+
+
+	HAL_StatusTypeDef status = LIS3MDL_ReadRegisters(dev, TEMP_OUT_L, regData[0], 1);
+	status = LIS3MDL_ReadRegisters(dev, TEMP_OUT_H, regData[1], 1);
+
+	/*
+	 * Combine register values to give raw temperature reading (16 bits two's complement)
+	 */
+	int16_t tempRaw = (((int16_t)regData[1] << 8) | (regData[0])); /* X-axis */
+
+
+	/* Convert deg C (offset @ 25 degC, slope = 8.0 LSB/degC) */
+	dev->temp_c = ((float)tempRaw / 256.0) + 25.0;
+
+	return status;
+
+}
+
+
+HAL_StatusTypeDef LIS3MDL_ReadMagnetometer(LIS3MDL *dev){
+	/*	DATASHEET PAGE 8 */
+
+	/*
+	 * Read raw values from magnetometer registers (16 bits two's complement)
+	 */
+	uint8_t regData[6];
+
+	HAL_StatusTypeDef status = LIS3MDL_ReadRegisters(dev, OUT_X_L, regData, 6);
+
+	/*
+	 * Combine register values  to give raw magnetometer reading (16 bits two's complement)
+	 */
+	int16_t magRawSigned[3];
+
+	magRawSigned[0] = ((int16_t) regData[1]<<8 |  regData[0]); /* X-axis */
+	magRawSigned[1] = ((int16_t) regData[3]<<8 |  regData[2]); /* Y-axis */
+	magRawSigned[2] = ((int16_t) regData[5]<<8 |  regData[4]); /* Z-axis */
+	
+
+	/* Converting to gauss (given range setting of +- 4 gauss) */
+	dev->mag_guass[0] = (float)magRawSigned[0] * 0.0001461560947f;
+	dev->mag_guass[1] = (float)magRawSigned[1] * 0.0001461560947f;
+	dev->mag_guass[2] = (float)magRawSigned[2] * 0.0001461560947f;
+
+	return status;
+
+}
+
+/*
+ * LOW-LEVEL FUNCTIONS
+ */
+HAL_StatusTypeDef LIS3MDL_ReadRegister(LIS3MDL *dev, uint8_t reg, uint8_t *data){
+
+	return HAL_I2C_Mem_Read( dev->i2cHandle, LISM3MDL_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
+
+}
+HAL_StatusTypeDef LIS3MDL_ReadRegisters(LIS3MDL *dev, uint8_t reg, uint8_t *data, uint8_t length){
+
+	return HAL_I2C_Mem_Read( dev->i2cHandle, LISM3MDL_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, length, HAL_MAX_DELAY);
+
+}
+
+HAL_StatusTypeDef LIS3MDL_WriteRegister(LIS3MDL *dev, uint8_t reg, uint8_t *data){
+
+	return HAL_I2C_Mem_Write( dev->i2cHandle, LISM3MDL_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
+
+}
+
+
---
+++ lis3mdl.h
@@ -0,0 +1,109 @@
+/*
+ *
+ * 	LIS3MDL Magnetometer I2C Driver
+ *
+ *	Author:	Mason Vari
+ *	Created: 12 October 2023
+ *
+ */
+
+/* Prevents recursive inclusion -------------------------------------*/
+#ifndef LIS3MDL_I2C_DRIVER_H
+#define LIS3MDL_I2C_DRIVER_H
+
+
+/* Includes ---------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+
+
+/*
+ * DEFINES
+ */
+#define LISM3MDL_I2C_ADDR		(0x1C << 1) /* SAD = 0 -> 0x38 = (0x1C << 1), SAD = 1 -> 0x3C  = (0x1E << 1) (p. 17)*/
+
+#define WHO_AM_I				0x0F
+#define LIS3MDL_DEVICE_ID		0x3D
+
+
+/*
+ * CONTROL REGISTERS (p. 23)
+ */
+#define CTRL_REG1				0x20
+#define CTRL_REG2				0x21
+#define CTRL_REG3				0x22
+#define CTRL_REG4				0x23
+#define CTRL_REG5				0x24
+
+/*
+ * STATUS REGISTER
+ */
+#define STATUS_REG				0x27
+
+/*
+ * OUTPUT REGISTERS
+ */
+#define OUT_X_L					0x28
+#define OUT_X_H					0x29
+#define OUT_Y_L					0x2A
+#define OUT_Y_H					0x2B
+#define OUT_Z_L					0x2C
+#define OUT_Z_H					0x2D
+#define TEMP_OUT_L				0x2E
+#define TEMP_OUT_H				0x2F
+
+/*
+ * INTERRUPT REGISTERS
+ */
+#define INT_CFG					0x30
+#define INT_SRC					0x31
+#define INT_THS_L				0x32
+#define INT_THS_H				0x33
+
+/*
+ * SENSOR STRUCT
+ */
+
+typedef struct {
+
+	/* I2C handle */
+	I2C_HandleTypeDef *i2cHandle;
+
+	/* Magnetometer dta (X, Y, Z) in gauss */
+	float mag_guass[3];
+
+	/* Temperature data in celsius */
+	float temp_c;
+
+} LIS3MDL;
+
+/*
+ * INITIALIZATION
+ */
+uint8_t	LIS3MDL_Initialize(LIS3MDL *dev, I2C_HandleTypeDef *i2cHandle);
+
+/*
+ * DATA AQUISITION
+ */
+HAL_StatusTypeDef LIS3MDL_ReadTemperature(LIS3MDL *dev);
+HAL_StatusTypeDef LIS3MDL_ReadMagnetometer(LIS3MDL *dev);
+
+
+/*
+ * LOW-LEVEL FUNCTIONS
+ */
+HAL_StatusTypeDef LIS3MDL_ReadRegister(LIS3MDL *dev, uint8_t reg, uint8_t *data);
+HAL_StatusTypeDef LIS3MDL_ReadRegisters(LIS3MDL *dev, uint8_t reg, uint8_t *data, uint8_t length);
+
+HAL_StatusTypeDef LIS3MDL_WriteRegister(LIS3MDL *dev, uint8_t reg, uint8_t *data);
+
+
+#endif
+
+
+
+
+
+
+
+
