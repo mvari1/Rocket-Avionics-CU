7342b59fabf3e125cfc621dccc9c131c27bb6c9b
mvari1 <144389101+mvari1@users.noreply.github.com>
2023-12-22 16:21:12 -0500

    Add files via upload
    Fixes to drivers

--- Drivers/MPL3115A2/mpl3115a2.c
+++ Drivers/MPL3115A2/mpl3115a2.c
@@ -2,6 +2,57 @@
 
 uint8_t	MPL3115A2_Initialize(MPL3115A2 *dev, I2C_HandleTypeDef *i2cHandle){
 
+	/* Set struct parameters */
+	dev->i2cHandle 		= i2cHandle;
+
+	dev->pressure_int	= 0;
+	dev->pressure_dec	= 0;
+
+	dev->altitude_int	= 0;
+	dev->altitude_dec	= 0;
+
+
+	dev->temp_c_int		= 0;
+	dev->temp_c_dec		= 0;
+
+	/* Store number of errors (to be returned at the end of the function) */
+	uint8_t	errNum = 0;
+	HAL_StatusTypeDef status;
+
+	/*
+	 * Initialization (pg. 14)
+	 */
+	uint8_t regData;
+	uint8_t STA = 0;
+
+	status = MPL3115A2_ReadRegister(dev, WHO_AM_I, &regData);
+	errNum += (status != HAL_OK);
+
+	if( regData != MPL3115A2_DEVICE_ID){
+
+		return 255;
+	}
+
+	do{
+
+		/* Enable Data Flags in PT_DATA_CFG */
+		regData = 0x07;
+		status = MPL3115A2_WriteRegister(dev, PT_DATA_CFG, &regData);
+		errNum += (status != HAL_OK);
+
+
+		/* Read STATUS Register */
+		MPL3115A2_ReadRegister(dev, DR_STATUS, &STA);
+		errNum += (status != HAL_OK);
+
+
+
+
+	}while( (STA & 0x08) != 0b00001000 );
+
+
+	return errNum;
+
 }
 
 
@@ -9,12 +60,87 @@ uint8_t	MPL3115A2_Initialize(MPL3115A2 *dev, I2C_HandleTypeDef *i2cHandle){
  * DATA AQUISITION
  */
 /*	DATASHEET PAGE  */
-HAL_StatusTypeDef MPL3115A2_ReadMagnetometer(MPL3115A2 *dev){
+HAL_StatusTypeDef MPL3115A2_ReadAltitude(MPL3115A2 *dev){
+
+	uint8_t regData[3];
+	uint8_t sensor_mode = ALTITUDE_MODE;
+	HAL_StatusTypeDef status;
+
+
+	/* Set Active Altitude */
+	status = MPL3115A2_WriteRegister(dev, CTRL_REG1, &sensor_mode);
+
+
+	/*
+	 * Read raw values from Altimeter registers (20 bits two's complement)
+	 */
+	status = MPL3115A2_ReadRegisters(dev, OUT_P_MSB, &regData, 3);
+
+
+
+	dev->altitude_int = (((int16_t) regData[0]  << 8) |  regData[1] );
+
+    dev->altitude_dec = regData[2] >> 4;
+
+
+
+
+	return status;
+
+
+}
+HAL_StatusTypeDef MPL3115A2_ReadPressure(MPL3115A2 *dev){
+
+	uint8_t regData[3];
+	int8_t off_H = 0;
+	uint8_t sensor_mode = PRESSURE_MODE;
+	HAL_StatusTypeDef status;
+
+
+	/* Set Active Pressure */
+	status = MPL3115A2_WriteRegister(dev, CTRL_REG1, &sensor_mode);
+
+
+	/*
+	 * Read raw values from Altimeter registers (20 bits two's complement)
+	 */
+	status = MPL3115A2_ReadRegisters(dev, OUT_P_MSB, &regData, 3);
+
+
+
+	dev->pressure_int = ((int32_t)(((int16_t) regData[0]  << 8) |  regData[1] ) << 2) | (regData[2] >> 6)  ;
+
+    dev->pressure_dec = (regData[2] & 0b00110000) >> 4;
+
+    status = MPL3115A2_ReadRegister(dev, OFF_H, &off_H);
+
+
+
+
+
+	return status;
+
+
 }
 
 
 /*	DATASHEET PAGE  */
 HAL_StatusTypeDef MPL3115A2_ReadTemperature(MPL3115A2 *dev){
+
+	uint8_t regData[2];
+	HAL_StatusTypeDef status;
+
+	/*
+	 * Read raw values from Temperature registers (12 bits two's complement)
+	 */
+	status = MPL3115A2_ReadRegisters(dev, OUT_T_MSB, &regData, 2);
+
+
+	dev->temp_c_int = regData[0];
+	dev->temp_c_dec = regData[1] >> 4;
+
+
+	return status;
 
 }
 
--- Drivers/MPL3115A2/mpl3115a2.h
+++ Drivers/MPL3115A2/mpl3115a2.h
@@ -19,14 +19,18 @@
 /*
  * DEFINES (pg. 19 - 20)
  */
-#define MPL3115A2_I2C_ADDR		(0x60 << 1)
+#define MPL3115A2_I2C_ADDR		(0xC0 )  /*Read (0xC1), Write (0xC0) */
 #define WHO_AM_I				0x0C	
+#define MPL3115A2_DEVICE_ID		0xC4
+#define PT_DATA_CFG				0x13
 #define SYSMOD					0x11
 
 /*
- * CONTROL REGISTERS (p. 23)
+ * CONTROL REGISTERS 
  */
 #define CTRL_REG1				0x26
+#define PRESSURE_MODE		 	0x39
+#define ALTITUDE_MODE			0xB9
 #define CTRL_REG2				0x27
 #define CTRL_REG3				0x28
 #define CTRL_REG4				0x29
@@ -39,6 +43,7 @@
 #define STATUS					0x00
 #define DR_STATUS				0x06  /*Check Data Sheet Based on Configuration*/
 #define F_STATUS				0x0D
+#define OFF_H 					0x2D  /* Off Set for Altitude Calculations */
 
 /*
  * OUTPUT REGISTERS 
@@ -53,6 +58,8 @@
 #define OUT_P_DELTA_LSB			0x09
 #define OUT_T_DELTA_MSB			0x0A
 #define OUT_T_DELTA_LSB			0x0B
+
+
 
 /*
  * INTERRUPT REGISTERS
@@ -69,11 +76,19 @@ typedef struct {
 	/* I2C handle */
 	I2C_HandleTypeDef *i2cHandle;
 
-	/* Altimeter data in kPA */
-	float pressure;
+	/* Pressure data in kPa */
+	uint32_t pressure_int;
+	uint8_t pressure_dec;
+
+	/* Pressure data in m */
+	int16_t altitude_int;
+	uint8_t altitude_dec;
+
 
 	/* Temperature data in celsius */
-	float temp_c;
+	int16_t temp_c_int;
+	uint8_t temp_c_dec;
+
 
 } MPL3115A2;
 
@@ -86,7 +101,8 @@ uint8_t	MPL3115A2_Initialize(MPL3115A2 *dev, I2C_HandleTypeDef *i2cHandle);
  * DATA AQUISITION
  */
 HAL_StatusTypeDef MPL3115A2_ReadTemperature(MPL3115A2 *dev);
-HAL_StatusTypeDef MPL3115A2_ReadMagnetometer(MPL3115A2 *dev);
+HAL_StatusTypeDef MPL3115A2_ReadAltitude(MPL3115A2 *dev);
+HAL_StatusTypeDef MPL3115A2_ReadPressure(MPL3115A2 *dev);
 
 
 /*
@@ -94,7 +110,6 @@ HAL_StatusTypeDef MPL3115A2_ReadMagnetometer(MPL3115A2 *dev);
  */
 HAL_StatusTypeDef MPL3115A2_ReadRegister(MPL3115A2 *dev, uint8_t reg, uint8_t *data);
 HAL_StatusTypeDef MPL3115A2_ReadRegisters(MPL3115A2 *dev, uint8_t reg, uint8_t *data, uint8_t length);
-
 HAL_StatusTypeDef MPL3115A2_WriteRegister(MPL3115A2 *dev, uint8_t reg, uint8_t *data);
 
 
